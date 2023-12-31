9bb2d56bee5888de1e6d8ffa5d48c8d69c8aa2a9
Keorapetse Fani <keorapetse.fani@dimensiondata.com>
2023-12-31 17:19:57 +0200

    Initial message for the first services

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -40,10 +40,30 @@ namespace AuctionService.Controllers
             .Include(x => x.Item)
             .FirstOrDefaultAsync(x => x.Id == id);
 
-            if(auction == null) return NotFound();
+            if (auction == null) return NotFound();
 
             return _mapper.Map<AuctionDto>(auction);
 
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Auction>> CreateAuction(CreateAuctionDto auctionDto)
+        {
+            var auction = _mapper.Map<Auction>(auctionDto);
+
+            // TODO: Add current user as seller
+            auction.Seller = "test";
+
+            Console.WriteLine(auction.ToString());
+
+            _context.Auctions.Add(auction);
+            _context.SaveChanges();
+
+            var results = await _context.SaveChangesAsync() > 0;
+
+            if (!results) return BadRequest("Could not save an auction to the database");
+
+            return CreatedAtAction(nameof(getAuctionById), new { auction.Id }, _mapper.Map<AuctionDto>(auction));
         }
     }
 }
