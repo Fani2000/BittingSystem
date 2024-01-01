68ac09762e25f8e06f2f78695cd8b87d4efe38ab
Keorapetse Fani <keorapetse.fani@dimensiondata.com>
2024-01-01 20:15:13 +0200

    Added the search service

---
+++ src/SearchService/Program.cs
@@ -0,0 +1,27 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Data;
+using SearchService.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
+app.Run();
