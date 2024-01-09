25d11581eca8b59c6d8797d9d8f92ad5df38ee3c
Keorapetse Fani <keorapetse.fani@dimensiondata.com>
2024-01-09 23:55:29 +0200

    Added the service gateway and also added the other consumers to the main app

---
+++ src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,34 @@
+﻿using AuctionService.Data;
+using AuctionService.Entities;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers
+{
+    public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+    {
+        private readonly AuctionDbContext context;
+
+        public AuctionFinishedConsumer(AuctionDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task Consume(ConsumeContext<AuctionFinished> context)
+        {
+            Console.WriteLine("---> Consuming Auction Finished");
+
+            var auction = await this.context.Auctions.FindAsync(context.Message.AuctionId);
+
+            if(context.Message.ItemSold)
+            {
+                auction.Winner = context.Message.Winner;
+                auction.SoldAmount = context.Message.Amount;
+            }
+
+            auction.Status = auction.SoldAmount > auction.ReservedPrice ? Status.Finished : Status.ReserveNotMet;
+
+            await this.context.SaveChangesAsync();
+        }
+    }
+}
